41b187b495b53aeb3c841937fb8cd4e26788cd03
lucas <lucasgominhaa448@gmail.com>
2021-05-09 14:39:46 -0300

    :fire: added new functions to calculate the IMC

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,52 @@ const form = document.querySelector('#form');
 
 form.addEventListener('submit',function(event){
     event.preventDefault();
-    console.log('event preventended');
+    
+    const inputWeight = event.target.querySelector('#weight');
+    const inputHeight = event.target.querySelector('#height');
+
+    
+
+    const weight = Number(inputWeight.value);
+    const height = Number(inputHeight.value);
+
+    if(!weight){
+        setResult('Weight invalid',false);
+        return;
+    }
+
+    if(!height){
+        setResult('Height invalid',false);
+        return;
+    }
+
+    const imc = getImc(weight,height);
+
+    console.log(imc);
+
 });
+
+    function getImc(weight,height) {
+        const imc = weight / height ** 2;
+        return imc.toFixed(2);
+    }
+        
+    
+
+function createP(){
+    const p = document.createElement('p');    
+    return p;
+}
+
+function setResult(msg, isValid){
+    const result = document.querySelector('#result');
+    result.innerHTML = '';
+
+
+    const p = createP();
+    p.innerHTML = msg;
+    result.appendChild(p);
+
+    
+}
 
